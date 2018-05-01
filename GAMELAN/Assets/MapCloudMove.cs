09f177179ca6dc99e00972018a4b8c62ac1b80d3
Farid Adi <farid.adi.ws@gmail.com>
2018-05-01 15:30:50 +0700

    Edit main dan map
    1. menambah fungsi gerak awan
    2. menambah shadow pada GUI dan text
    3. Merisize ukuran button

---
+++ GAMELAN/Assets/MapCloudMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCloudMove : MonoBehaviour
+{
+    public Rigidbody2D cloud;
+    public float speed;
+    public float maxMove;
+    private float time;
+    private bool flip = false;
+    private void Start()
+    {
+        cloud.velocity = new Vector2(speed,0);
+    }
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (maxMove < time)
+        {
+            time = 0;
+            if (flip == false)
+            {
+                flip = true;
+                cloud.velocity = new Vector2(-speed, 0);
+            }
+            else
+            {
+                flip = false;
+                cloud.velocity = new Vector2(speed, 0);
+            }
+            
+        }
+    }
+}
+ 
---
+++ GAMELAN/Assets/ScrollCloud.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollCloud : MonoBehaviour
+{
+    public GameObject bg1;
+    public GameObject bg2;
+    public float speed;
+    private static Rigidbody2D rb21;
+    private static Rigidbody2D rb22;
+    public static ScrollBackground Sb;
+    // Use this for initialization
+    void Start()
+    {
+        rb21 = bg1.GetComponent<Rigidbody2D>();
+        rb21.velocity = new Vector2(-speed, 0);
+        rb22 = bg2.GetComponent<Rigidbody2D>();
+        rb22.velocity = new Vector2(-speed, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void updateSpeed(float scrollSpeed)
+    {
+        rb21.velocity = new Vector2(-scrollSpeed, 0);
+        rb22.velocity = new Vector2(-scrollSpeed, 0);
+    }
+}
+
