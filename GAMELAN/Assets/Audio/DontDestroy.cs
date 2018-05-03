7ba3364be1ad466541d6fb6e108392542bf70fbf
yusrilAditya98 <adit9b02@gmail.com>
2018-05-03 13:10:46 +0700

    nambah sound

---
+++ GAMELAN/Assets/Audio/DontDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}
