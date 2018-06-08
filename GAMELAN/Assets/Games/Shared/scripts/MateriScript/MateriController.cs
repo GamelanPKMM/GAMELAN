6fce863be5a85b0bed05b7e1899f7cacdd71c5c9
edho08 <edho08@gmail.com>
2018-06-08 22:41:11 +0700

    Menambahkan fitur exciting pada materi

--- GAMELAN/Assets/Games/Shared/scripts/MateriScript/MateriController.cs
+++ GAMELAN/Assets/Games/Shared/scripts/MateriScript/MateriController.cs
@@ -2,24 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using System;
 
 public class MateriController : MonoBehaviour {
+    public float waitTime = 4;
     public static MateriController self;
     public GameObject parent;
     public Image gambar;
     public Text penjelasan;
     public Button next;
     public Button prev;
+    public Scrollbar scroll;
+    public Text timer;
     private Text next_text;
     private GamesMateri materis;
-    private int current = 0;
-    private int lastSaw = 0;
-    private int last = 0;
+    public int current = 0;
+    public int lastSaw = -1;
+    public int last = 0;
     private string materiPath = null;
     private float dt = 0;
     private float time;
     private MiniGameLoaderScript m;
+    private Coroutine waiting;
+    private UnityEvent scrollOnValueChanged = new UnityEvent();
     // Use this for initialization
 
     private void Awake()
@@ -35,7 +41,7 @@ public class MateriController : MonoBehaviour {
     }
 
     void Start() {
-        
+        //scroll.onValueChanged.AddListener(delegate { scrollOnValueChanged.Invoke(); });
         self = this;
         parent.SetActive(false);
         next.onClick.AddListener(Next);
@@ -43,12 +49,6 @@ public class MateriController : MonoBehaviour {
         next_text = next.transform.GetChild(0).gameObject.GetComponent<Text>();
     }
 
-    private void Update()
-    {
-        if (Time.time - time > dt || current < lastSaw) {
-            next.transform.gameObject.SetActive(true);
-        }
-    }
     public void startMateri() {
         dt = 0;
         start();
@@ -72,24 +72,24 @@ public class MateriController : MonoBehaviour {
         prev.transform.gameObject.SetActive(false);
         materis = GamesMateri.load(materiPath);
         current = 0;
+        lastSaw = -1;
         last = materis.materis.Count;
         time = Time.time;
         next_text.text = "Lanjut";
+        next.transform.gameObject.SetActive(false);
         showmateri(current);
     }
     void Next() {
-        
         if (current == last - 2)
         {
             next_text.text = "Main";
         }
         if (current < last-1)
         {
-            lastSaw += current == lastSaw ? 1 : 0;
-            showmateri(++current);
-            time = Time.time;
             next.transform.gameObject.SetActive(false);
             prev.transform.gameObject.SetActive(true);
+            showmateri(++current);
+            time = Time.time;
         }
         else finish();
     }
@@ -97,24 +97,48 @@ public class MateriController : MonoBehaviour {
         if (current > 0)
         {
             next_text.text = "Lanjut";
+            StopCoroutine(waiting);
+            timer.text = "";
+            next.transform.gameObject.SetActive(true);
             showmateri(--current);
         }
        
     }
 
     void showmateri(int i) {
-        penjelasan.text = materis.materis[i].Penjelasan; try
+        penjelasan.text = materis.materis[i].Penjelasan;
+        scroll.value = 1;
+        try
         {
             gambar.sprite = Resources.Load<Sprite>("Materi/Gambar/" + materis.materis[i].imageName);
         }
         catch (NullReferenceException e) {
-            Debug.Log("File cannot be found. AT "+System.DateTime.Now);
+            Debug.Log("File cannot be found. AT " + System.DateTime.Now);
         }
-        if(current <= 0)
+        if (current <= 0)
         {
             prev.transform.gameObject.SetActive(false);
         }
         Debug.Log("Materi/Gambar/" + materis.materis[i].imageName);
+        if (lastSaw <= current)
+        {
+            waiting = StartCoroutine(wait());
+            lastSaw = current;
+            //next.transform.gameObject.SetActive(false);
+            /*
+            if (!scroll.gameObject.activeSelf && scroll.size >=0.9)
+            {
+                scrollOnValueChanged.RemoveListener(checkForScroll);
+            }
+            else
+            {
+                scrollOnValueChanged.AddListener(checkForScroll);
+            }*/
+        }
+        else {
+            next.transform.gameObject.SetActive(true);
+
+        }
 
     } 
     void finish() {
@@ -124,5 +148,24 @@ public class MateriController : MonoBehaviour {
             m.gotoMiniGames();
         }
         else m = null;
+    }
+    /*
+    public void checkForScroll() {
+        if (scroll.value == 0) {
+            next.transform.gameObject.SetActive(true);
+            timer.text = "";
+            StopCoroutine(waiting);
+            scrollOnValueChanged.RemoveListener(checkForScroll);
+        }
+    }*/
+
+    IEnumerator wait() {
+        int i = 0;
+        while (i <= waitTime) {
+            timer.text = "" + ((int)(waitTime - i));
+            yield return new WaitForSecondsRealtime(1);
+            i++;
+        }
+        next.transform.gameObject.SetActive(true);
     }
 }
---
+++ GAMELAN/Assets/Games/Shared/scripts/RunningText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RunningText : MonoBehaviour{
+
+    public static void runningText(string stext, float dur, Text text) {
+        RunningText run = new RunningText();
+        run.StartCoroutine(runText(stext, dur, text));
+    }
+
+    static IEnumerator runText(string stext, float dur, Text text) {
+        float dTime = dur / stext.Length;
+        for (int i = 0; i < stext.Length; i++) {
+            text.text += stext[i];
+            yield return new WaitForSecondsRealtime(dTime);
+        }
+    }
+	
+}
